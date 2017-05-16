823486e1ebaf587dfcb1a72608618ad85955cb37
Valentin.Mueller <Valentin.Mueller@fdmgroup.com>
2017-05-16 16:20:06 +0100

    strings

--- src/main/java/com/fdmgroup/TddFizzBuzz/App.java
+++ src/main/java/com/fdmgroup/TddFizzBuzz/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(DaysOfTheWeekEnglish.Monday.getNumberOfDay());
+//        System.out.println(DaysOfTheWeekEnglish.Monday.getNumberOfDay());
+        
+        String[] strings = {"Foo", "Bar", "list"};
+        System.out.println(strings[0]);
+        int[] ints = new int[7];
+        int[] intss = {1,2,3};
+        int[][] intsq = new int[3][3];
+        intsq[0][0] = 1;
     }
 }
